5fdd2ffdc4d42cf8b9dcad267c33a32e65c97367
sleepinGiant <shukla.ayush0203@gmail.com>
2023-07-05 02:15:06 +0530

    User Register, Login

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 // Middleware Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -35,3 +37,25 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// Routes with Files
+app.post("/auth/register", upload.single("picture"), register);
+
+// Routes
+app.use("/auth", authRoutes);
+
+// Mongoose Setup
+const PORT = process.env.PORT || 6001;
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server is running on port: ${PORT}`);
+		});
+	})
+	.catch((err) => {
+		console.log(`${err} did not connect`);
+	});
